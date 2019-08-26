b01d7a79d3a9c94b8b65208365fb146b52dd9ce9
Toyotomius <Toyotomius@gmail.com>
2019-08-26 19:51:59 -0300

    Added misc project for one-off scripts.

---
+++ Misc/Program.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Misc
+{
+    /// <summary>
+    /// For all miscellaneous one-off scripts and jobs that I need to do.
+    /// </summary>
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+        }
+    }
+}
